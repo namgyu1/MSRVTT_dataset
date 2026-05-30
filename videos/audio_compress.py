6fcd6d84f5967b1fb04cf5f842c54b9363e7dc23
namgyu1 <namgyu@postech.ac.kr>
2026-05-30 13:06:42 +0900

    Fix audio compression output path handling

--- videos/audio_compress.py
+++ videos/audio_compress.py
@@ -1,14 +1,21 @@
+import os
 import torchaudio
 import glob
 import torch
 
-audio_list = sorted(glob.glob('./audios_all/*'))
-for i in audio_list:
-        audio, sr = torchaudio.load(i)
-        downsample_resample = torchaudio.transforms.Resample(
-            sr, 16000, resampling_method='sinc_interpolation')
+input_dir = './audios_all'
+output_dir = './audio_all_compressed'
 
-        audio_down = downsample_resample(audio)
-        torchaudio.save('./audio_all_compressed'+i[12:], torch.clamp(audio_down, -1, 1), 16000, precision=32)
+os.makedirs(output_dir, exist_ok=True)
+
+audio_list = sorted(glob.glob(os.path.join(input_dir, '*')))
+for i in audio_list:
+    audio, sr = torchaudio.load(i)
+    downsample_resample = torchaudio.transforms.Resample(
+        sr, 16000, resampling_method='sinc_interpolation')
+
+    audio_down = downsample_resample(audio)
+    output_path = os.path.join(output_dir, os.path.basename(i))
+    torchaudio.save(output_path, torch.clamp(audio_down, -1, 1), 16000, precision=32)
 
  
